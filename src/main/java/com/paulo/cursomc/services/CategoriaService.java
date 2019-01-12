ada20050c321a10ba804db616def626bbdc4bf6a
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-01-12 19:21:05 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/paulo/cursomc/services/CategoriaService.java
+++ src/main/java/com/paulo/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.paulo.cursomc.domain.Categoria;
+import com.paulo.cursomc.dto.CategoriaDTO;
 import com.paulo.cursomc.repositories.CategoriaRepository;
 import com.paulo.cursomc.services.exceptions.DataIntegrityException;
 import com.paulo.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
