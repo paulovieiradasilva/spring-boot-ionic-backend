bd3ab4e41979ef206d8e9f8aa1c471963ab4697e
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-01-12 13:53:53 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/paulo/cursomc/Application.java
+++ src/main/java/com/paulo/cursomc/Application.java
@@ -13,6 +13,7 @@ import com.paulo.cursomc.domain.Cidade;
 import com.paulo.cursomc.domain.Cliente;
 import com.paulo.cursomc.domain.Endereco;
 import com.paulo.cursomc.domain.Estado;
+import com.paulo.cursomc.domain.ItemPedido;
 import com.paulo.cursomc.domain.Pagamento;
 import com.paulo.cursomc.domain.PagamentoComBoleto;
 import com.paulo.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.paulo.cursomc.repositories.CidadeRepository;
 import com.paulo.cursomc.repositories.ClienteRepository;
 import com.paulo.cursomc.repositories.EnderecoRepository;
 import com.paulo.cursomc.repositories.EstadoRepository;
+import com.paulo.cursomc.repositories.ItemPedidoRepository;
 import com.paulo.cursomc.repositories.PagamentoRepository;
 import com.paulo.cursomc.repositories.PedidoRepository;
 import com.paulo.cursomc.repositories.ProdutoRepository;
@@ -43,19 +45,21 @@ public class Application implements CommandLineRunner {
 
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -92,35 +96,48 @@ public class Application implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
+
 		Cliente cli1 = new Cliente(null, "Maria", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
-		
-		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
-		
+
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apt 303", "Jardim", "3822084", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 803", "Centro", "38777012", cli1, c2);
-		
+
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-		
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-		
+
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
-		
+
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
-		
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
 		ped2.setPagamento(pagto2);
-		
+
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 
--- src/main/java/com/paulo/cursomc/domain/Produto.java
+++ src/main/java/com/paulo/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.paulo.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -29,6 +32,9 @@ public class Produto implements Serializable {
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
+	
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Produto() {
 	}
@@ -38,6 +44,14 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos() {
+		List<Pedido> list = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			list.add(x.getPedido());
+		}
+		return list;
 	}
 
 	public Integer getId() {
@@ -72,6 +86,14 @@ public class Produto implements Serializable {
 		this.categorias = categorias;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -96,5 +118,4 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-
 }
