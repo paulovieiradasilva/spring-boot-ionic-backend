3d0a5b133f7fb55187a62c04b07b25db0a397614
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-01-12 22:09:29 -0300

    Ajustando e testando a delecao de Clientes

--- src/main/java/com/paulo/cursomc/services/ClienteService.java
+++ src/main/java/com/paulo/cursomc/services/ClienteService.java
@@ -63,7 +63,7 @@ public class ClienteService {
 			clienteRepository.deleteById(id);
 
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionadas");
 		}
 	}
 
