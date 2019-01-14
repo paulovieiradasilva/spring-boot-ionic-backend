79000bc20737106c67efc5ea1f150f70d1377801
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-01-14 10:56:26 -0300

    Implementando toStringo do Pedido

--- src/main/java/com/paulo/cursomc/services/PedidoService.java
+++ src/main/java/com/paulo/cursomc/services/PedidoService.java
@@ -32,6 +32,9 @@ public class PedidoService {
 
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = pedidoRepository.findById(id);
@@ -42,6 +45,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -59,7 +63,7 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-
+		System.out.println(obj);
 		return obj;
 	}
 }
