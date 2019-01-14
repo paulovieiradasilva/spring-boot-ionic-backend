1dd9525017a7dc86acfedc71576959b0a4917bff
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-01-14 00:12:32 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/paulo/cursomc/App.java
+++ src/main/java/com/paulo/cursomc/App.java
@@ -5,15 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class App implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 
 	}
-
+	
 }
