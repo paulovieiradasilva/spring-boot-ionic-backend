8843b28a13390337e502cb82a302b0c82418630f
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-01-16 22:13:50 -0300

    Email HTML

--- src/main/java/com/paulo/cursomc/services/EmailService.java
+++ src/main/java/com/paulo/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.paulo.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.paulo.cursomc.domain.Pedido;
@@ -9,4 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido pedido);
 
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
+
 }
