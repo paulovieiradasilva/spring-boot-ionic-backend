206937c260d45ee4bda03186d4801b69e37e6562
paulovieiradasilva <paulovieiradasilva@hotmail.com.br>
2019-01-17 23:50:20 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/paulo/cursomc/App.java
+++ src/main/java/com/paulo/cursomc/App.java
@@ -1,11 +1,17 @@
 package com.paulo.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.paulo.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class App implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
@@ -13,7 +19,7 @@ public class App implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("/home/paulo-vieira/Imagens/foto.jpeg");
 	}
 	
 }
